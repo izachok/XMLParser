85838f694303091dc3a3ff3d53701fbf41a4c374
Olga Tiutiunnyk <izachok@gmail.com>
2021-05-03 19:47:14 +0300

    Add project files.

---
+++ XMLParser/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace XMLParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string directoryPath = "";
+            string parentNodeName = "";
+            bool includeParentNode = false;
+            string outputFilePath = "";
+
+            Console.WriteLine("Enter full path to directory with xml files:");
+            directoryPath = Console.ReadLine();
+
+            Console.WriteLine("Enter parent node's name:");
+            parentNodeName = Console.ReadLine();
+
+            Console.WriteLine("Include parent node to output file?(y - yes, n - no):");
+            char key = Console.ReadKey().KeyChar;
+            includeParentNode = (key == 'y' || key == 'Y') ? true : false;
+
+            Console.WriteLine("\nEnter output file name with full path:");
+            outputFilePath = Console.ReadLine();
+
+            //ProcessFilesInDirectory(@"d:\IT\Code\XMLParser\XMLParser\XML", "HotSpots", 
+            //    true, @"d:\IT\Code\XMLParser\XMLParser\result.xml");
+
+            //XMLParser parser = new XMLParser(@"d:\IT\Code\XMLParser\XMLParser\XML", "Panoramas", 
+            //    true, @"d:\IT\Code\XMLParser\XMLParser\result.xml");
+            XMLParser parser = new XMLParser(directoryPath, parentNodeName, includeParentNode, outputFilePath);
+            parser.ProcessFilesInDirectory();
+            //ProcessFilesInDirectory(directoryPath, parentNodeName, includeParentNode, outputFilePath);
+            Console.WriteLine("Press any key...");
+            Console.Read();
+        }
+    }
+}
---
+++ XMLParser/XMLParser.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Xml;
+
+namespace XMLParser
+{
+    class XMLParser
+    {
+        public string DirectoryPath { get; set; }
+        public string ParentNodeName { get; set; }
+        public bool IncludeParentNode { get; set; }
+        public string OutputFileName { get; set; }
+
+        public XMLParser(string directoryPath, string parentNodeName, bool includeParentNode, string outputFileName)
+        {
+            DirectoryPath = directoryPath;
+            ParentNodeName = parentNodeName;
+            IncludeParentNode = includeParentNode;
+            OutputFileName = outputFileName;
+        }
+
+        public void ProcessFilesInDirectory()
+        {
+            StringBuilder resultXML = new StringBuilder();
+            try
+            {
+                var xmlFiles = Directory.EnumerateFiles(DirectoryPath, "*.xml");
+
+                foreach (string currentFile in xmlFiles)
+                {
+                    resultXML.Append(FindXMLNode(currentFile).Trim()).Append("\n");
+                }
+                SaveXMLToFile(resultXML.ToString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        private string FindXMLNode(string fullPath)
+        {
+            XmlReaderSettings settings = new XmlReaderSettings();
+            settings.ConformanceLevel = ConformanceLevel.Fragment;
+            string result = "";
+
+            using (XmlReader reader = XmlReader.Create(fullPath, settings))
+            {
+                while (reader.Read())
+                {
+                    if (reader.IsStartElement(ParentNodeName))
+                    {
+                        result = IncludeParentNode ? reader.ReadOuterXml() : reader.ReadInnerXml();
+                    }
+                }
+            }
+            return result;
+        }
+
+        private void SaveXMLToFile(string xml)
+        {
+            
+            File.WriteAllText(OutputFileName, xml, Encoding.UTF8);
+        }
+    }
+}
